a8c2b5c70f1c2cac7c4709099ca8ddd4d817c314
Matt Kelly <bmatthewkelly@gmail.com>
2022-06-07 16:43:29 -0700

    more config troubleshooting

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,20 @@
 import adapter from '@sveltejs/adapter-auto';
-import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
+// import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  experimental: {
+    prebundleSvelteLibraries: true,
+  },
   kit: {
     adapter: adapter(),
     vite: () => ({
-      optimizeDeps: {
-        esbuildOptions: {
-          plugins: [esbuildCommonjs(['@sendgrid'])],
-        },
-        // exclude: ['path', 'fs', 'os', 'perf_hooks', 'util'],
-      },
+      // optimizeDeps: {
+      //   esbuildOptions: {
+      //     plugins: [esbuildCommonjs(['@sendgrid'])],
+      //   },
+      //   // exclude: ['path', 'fs', 'os', 'perf_hooks', 'util'],
+      // },
       server: {
         fs: {
           allow: ['..'],
