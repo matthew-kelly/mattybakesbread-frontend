01462d74bc5a9caf826382871292339a9eb7e02b
Matt Kelly <bmatthewkelly@gmail.com>
2022-06-08 14:46:19 -0700

    fix phone number in admin email

--- src/routes/api/order/sendAdminEmail.js
+++ src/routes/api/order/sendAdminEmail.js
@@ -162,7 +162,7 @@ table.body .article {
                           Email: ${order.email}
                         </p>
                         <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;">
-                          Phone: ${formatPhoneNumber(order.phone)}
+                          Phone: ${formatPhoneNumber(order.phoneNumber)}
                         </p>
                         <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;">
                           Address: ${order.address} ${order.address2}, ${order.city}
