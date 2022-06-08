580d5445fb6f696108c236461a39da5e118833fe
Matt Kelly <bmatthewkelly@gmail.com>
2022-06-07 17:48:09 -0700

    solved config issue, cloudflare pages runs on v8 not node, reverting to original setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,10 @@
 import adapter from '@sveltejs/adapter-auto';
-import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // experimental: {
-  //   prebundleSvelteLibraries: true,
-  // },
   kit: {
     adapter: adapter(),
     vite: () => ({
-      optimizeDeps: {
-        esbuildOptions: {
-          plugins: [esbuildCommonjs(['@sendgrid/mail'])],
-        },
-        exclude: ['path', 'fs', 'os', 'perf_hooks', 'util'],
-        // include: ['@sendgrid/mail'],
-      },
       server: {
         fs: {
           allow: ['..'],
